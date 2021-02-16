93fc1f880f5c3b8b116bfa7492baf1e937b3af5b
guiguyudie <myfamilyiswell@outlook.com>
2021-02-16 22:35:36 +0800

    Signed-off-by:kr <myfamilyiswell@outlook.com>

---
+++ mavel.cpp
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()  
+//{
+//
+//	int age = 20;
+//	int size = 40;
+//	printf(" age = %d", size);
+//
+//
+//return 0;
+
+//#include <stdio.h>
+//	当局部变量和全局变量同名时，局部变量优先
+//int globle = 80;
+//int main()
+//{ 
+//
+//	int size = 40;
+//	int globle = 99;
+//		printf("sum =  %d\n%d\n", size, globle);
+//
+//
+//	return 0;
+
+//#include <stdio.h>
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//		int sum = 0;
+//		printf("输入两个操作数:>");
+//	scanf("%d %d", &num1, &num2);
+//	sum = num1 + num1;
+//	printf("sum = %d\n", sum);
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	char ch = 'n';
+//
+//	printf("%c\n", ch);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>			
+//int main()
+//{
+//const int b = 10;  const定义的常变量，不能再被赋值了，该程序有误
+//b = 7;
+//printf("%d\n", b);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int c = 9;
+//	 
+//	printf("平均值 = %d\n", c = 10);
+//
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char arr1 [] = "abc";
+//	// char arr2[] = { 'a', 'b', 'c',0 };         /0时字符结束标志，不做内容
+//	printf("一组字符串：%s\n", arr1);
+//		printf("一组字符串：%s\n", arr2);
+//
+//	return 0;
+//}
